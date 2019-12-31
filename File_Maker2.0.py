ea0dd7c1ae8091751be688ac87f07d8beb64d9cb
Ankit Gutpa <gankit389@gmail.com>
2019-12-31 15:07:40 -0500

    final commit

--- File_Maker2.0.py
+++ File_Maker2.0.py
@@ -1,11 +1,15 @@
 from tkinter import *
 from tkinter.scrolledtext import ScrolledText
+import os
+from random import randint
+import datetime
+import shutil
 
 root= Tk()
 root.resizable(False , False)
 root.geometry("450x250+500+150")
 
-Label(root, text= "   Welcome to File Maker", font = "Helvetica 24").place(x = 29, y = 0)
+Label(root, text= "Welcome to File Maker", font = "Helvetica 24").place(x = 50, y = 0)
 Label(root, text="File name").place(x = 0, y = 44)
 Label(root, text="Name").place(x = 0, y = 74)
 Label(root, text="Date").place(x = 0, y = 104)
@@ -21,13 +25,35 @@ Author.place(x = 87, y = 75, height = 23)
 Date.place(x = 87, y = 104, height = 23)
 Description.place(x = 86, y = 135)
 
-fileName = Button(root, text="Make a random name")
-defualtName = Button(root, text ="Use the default name")
-defualtDate = Button(root, text ="Use today's date")
-create = Button(root, text = "create file")
+def file_call():
+    fileName.insert(0,"file"+str(randint(0,999))+".py")
 
-fileName.place(x = 278, y = 44, width = 130)
-defualtName.place(x = 278, y = 73, width = 130)
+def Name_call():
+    Author.insert(0,"Ankit Gupta")
+
+def Date_call():
+    today = datetime.date.today()
+    Date.insert(0, today.strftime("%A,%B %d, %Y"))
+
+def Create():
+    fileAuthor = "# Author:      "+ Author.get()
+    fileDate = "# Date:        " + Date.get()
+    fileDescription = "# Description: " + Description.get(1.0,END)
+
+    f = open( "C:/Users/ganki/Desktop/python/personal_projects/" + fileName.get() + ".py", "w+")
+    f.write(fileAuthor + "\n")
+    f.write(fileDate + "\n")
+    f.write(fileDescription + "\n\n\n\n")
+    f.close()
+    root.destroy()
+
+randomFile = Button(root, text="Make a random name", command = file_call)
+defualtAuthor = Button(root, text ="Use the default name", command = Name_call)
+defualtDate = Button(root, text ="Use today's date", command = Date_call)
+create = Button(root, text = "create file", command = Create)
+
+randomFile.place(x = 278, y = 44, width = 130)
+defualtAuthor.place(x = 278, y = 73, width = 130)
 defualtDate.place(x = 278, y = 102, width = 130)
 create.place(x = 120, y = 215, width = 200)
 
